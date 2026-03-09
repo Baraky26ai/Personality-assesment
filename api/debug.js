b459680a79d6817641d45687939afa005b707ee2
“Barak <baraky26@gmail.com>
2026-03-09 17:23:45 +0200

    Add debug endpoint

---
+++ api/debug.js
@@ -0,0 +1,21 @@
+module.exports = async function handler(req, res) {
+  const env = {
+    hasSheetId: !!process.env.GOOGLE_SHEET_ID,
+    hasEmail: !!process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    hasKey: !!process.env.GOOGLE_PRIVATE_KEY,
+    keyStart: (process.env.GOOGLE_PRIVATE_KEY || '').substring(0, 30),
+    keyHasRealNewlines: (process.env.GOOGLE_PRIVATE_KEY || '').includes('\n'),
+    keyHasEscapedNewlines: (process.env.GOOGLE_PRIVATE_KEY || '').includes('\\n'),
+  };
+
+  try {
+    const { getSheet } = require('./_lib/sheets');
+    const doc = await getSheet();
+    env.sheetsConnected = !!doc;
+    env.sheetTitle = doc ? doc.title : null;
+  } catch (e) {
+    env.sheetsError = e.message;
+  }
+
+  res.json(env);
+};
